a3ad3b125fdb6cd3cf35c459173f979d95024f5c
HyechurnJang <hyjang@cisco.com>
2018-05-11 11:03:43 +0900

    update

--- adapter.py
+++ adapter.py
@@ -12,13 +12,19 @@ APIC_IP = '10.72.86.21'
 APIC_USERNAME = 'admin'
 APIC_PASSWORD = '1234Qwer'
 
+FILTER_EPG = 'tn-SDS-Tenant-01/ap-APP-01/epg-WAS'
+
 ctrl = Controller(APIC_IP, APIC_USERNAME, APIC_PASSWORD)
 
 class Genian(Event):
     
-    def handle(self, status, obj):
+    def filter(self, status, obj):
+        if FILTER_EPG not in obj['dn']: return
         print status
         print json.dumps(obj, indent=2)
+    
+    def handle(self, status, obj):
+        self.filter(status, obj)
 
 ctrl.Endpoint.event(Genian())
 
