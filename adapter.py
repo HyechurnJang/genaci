9aa2eca84920f6fb2a6583e0794483340bd5826a
HyechurnJang <hyjang@cisco.com>
2018-05-11 12:22:19 +0900

    update

--- adapter.py
+++ adapter.py
@@ -5,6 +5,7 @@ Created on 2018. 5. 11.
 '''
 
 import json
+import requests
 from pygics import Task
 from acidipy import Controller, Event
 
@@ -13,21 +14,62 @@ APIC_USERNAME = 'admin'
 APIC_PASSWORD = '1234Qwer'
 APIC_DEBUG = True
 
+GEN_IP = '192.168.2.100'
+GEN_KEY = '36854bcd-6083-4701-b2c9-9e545048c26d'
+
 FILTER_EPG = 'tn-SDS-Tenant-01/ap-APP-01/epg-WAS'
 
 ctrl = Controller(APIC_IP, APIC_USERNAME, APIC_PASSWORD, debug=APIC_DEBUG)
 
 class Genian(Event):
     
-    def filter(self, status, obj):
-        if FILTER_EPG not in obj['dn']: return
-        print 'ENDPOINT EVENT : %s' % status
-        print json.dumps(obj, indent=2)
-        print ''
+    def __init__(self, ip, key):
+        self.ip = ip
+        self.key = key
+        self.url = 'https://%s:443/mc2/rest/nodes/?apiKey=%s' % (self.ip, self.key)
+        self.headers = {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        }
     
     def handle(self, status, obj):
         self.filter(status, obj)
+    
+    def filter(self, status, obj):
+        if FILTER_EPG not in obj['dn']: return
+        if status == 'deleted': return
+        if 'ip' not in obj: return
+        if 'mac' not in obj: return
+        if obj['ip'] == '0.0.0.0': return
+        mac = obj['mac']
+        ip = obj['ip']
+        print 'ENDPOINT EVENT : %s > %s / %s' % (status, mac, ip)
+        self.forward(mac, ip)
+    
+    def forward(self, mac, ip):
+        try:
+            data = [{
+                'nl_ipstr' : mac,
+                'nl_mac' : ip,
+                'nl_sensornid' : '',
+                'nl_genidev' : 0,
+                'doNotDeleteNode' : False
+            }]
+            resp = requests.post(
+                self.url,
+                headers=self.headers,
+                data=json.dumps(data),
+                verify=False
+            )
+            print 'Genian Status : %d' % resp.status_code
+            if resp.status_code != 200: return False
+            print json.dumps(resp.json(), indent=2)
+            print ''
+        except Exception as e:
+            print 'Error : %s' % str(e)
+            return False
+        return True
 
-ctrl.Endpoint.event(Genian())
+ctrl.Endpoint.event(Genian(GEN_IP, GEN_KEY))
 
 Task.idle()
--- test.py
+++ test.py
@@ -7,9 +7,11 @@ Created on 2018. 5. 11.
 import json
 import requests
 
-IP = ''
-KEY = ''
+TEST_IP = '192.168.2.'
+TEST_MAC = 'ab:cd:ef:ab:cd:ef'
 
+IP = '192.168.2.100'
+KEY = '36854bcd-6083-4701-b2c9-9e545048c26d'
 url = 'https://%s:443/mc2/rest/nodes/?apiKey=%s' % (IP, KEY)
 
 headers = {
@@ -18,8 +20,8 @@ headers = {
 }
 
 data = [{
-    'nl_ipstr' : '',
-    'nl_mac' : '',
+    'nl_ipstr' : TEST_IP,
+    'nl_mac' : TEST_MAC,
     'nl_sensornid' : '',
     'nl_genidev' : 0,
     'doNotDeleteNode' : False
@@ -27,9 +29,11 @@ data = [{
 
 resp = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
 print resp.status_code
-
 if resp.status_code != 200:
     print 'Error'
     exit(1)
 
 data = resp.json()
+
+print json.dumps(data, indent=2)
+
